e4dd1f6dd7dd014a9c4ad1b75038eacb91ddd3a3
Louis <louisreynard919@gmail.com>
2022-06-22 10:42:13 +0200

    :construction: changed for ItemMeta

--- src/main/java/fr/ayato/infiniteitems/listeners/EventListener.java
+++ src/main/java/fr/ayato/infiniteitems/listeners/EventListener.java
@@ -1,10 +1,8 @@
 package fr.ayato.infiniteitems.listeners;
 
-import de.tr7zw.nbtapi.NBTItem;
 import fr.ayato.infiniteitems.Main;
 import fr.ayato.infiniteitems.items.CreateItem;
 import fr.ayato.infiniteitems.utils.Config;
-import fr.ayato.infiniteitems.utils.Utils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -32,35 +30,6 @@ public class EventListener implements Listener {
     public void checkOnDeath(PlayerDeathEvent e) {
         final UUID playerUUID = e.getEntity().getPlayer().getUniqueId();
         final Iterator<ItemStack> iterator = e.getDrops().iterator();
-        final Player killer = e.getEntity().getKiller();
-        List<String> lore = new ArrayList<>();
-
-        if (killer != e.getEntity()) {
-            ItemStack killerItem = killer.getItemInHand();
-            NBTItem nbtItem = new NBTItem(killerItem);
-            if (nbtItem.hasKey("infinite")) {
-                Material material = killerItem.getType();
-                switch (material) {
-                    case DIAMOND_SWORD:
-                        ItemStack itemStack = killer.getItemInHand();
-                        nbtItem.getItem();
-                        nbtItem.setInteger("kills", nbtItem.getInteger("kills") + 1);
-                        nbtItem.applyNBT(itemStack);
-                        List<String> loreFromConfig = Config.getItemLore("sword");
-                        for (String s : loreFromConfig) {
-                            if (s.contains("%kills%")) {
-                                s = s.replace("%kills%", String.valueOf(nbtItem.getInteger("kills")));
-                            }
-                            lore.add(s);
-                        }
-                        ItemMeta itemMeta = killerItem.getItemMeta();
-                        itemMeta.setLore(lore);
-                        itemStack.setItemMeta(itemMeta);
-                        killer.updateInventory();
-                        break;
-                }
-            }
-        }
 
         // Iterate over all the player inventory to check if he has infinite item
         while(iterator.hasNext()){
@@ -69,37 +38,36 @@ public class EventListener implements Listener {
             // If the item is an infinite item, remove it from the inventory and add it to the playerList
             for (int i = 0; i < Main.configItemName.size(); i++) {
                 final String configItem = Main.configItemName.get(i);
-                final List<String> configItemLore = Config.getItemLore(configItem.replace("items.", ""));
+                String confItem = configItem.replace("items.", "");
+                final List<String> configItemLore = Config.getItemLore(confItem);
                 final List<String> droppedItemLore = actualItemIterated.getItemMeta().getLore();
 
                 // Searching for infinite items
-                if (configItemLore.contains(droppedItemLore.get(1))) {
+                if (configItemLore.equals(droppedItemLore)) {
                     if (playerList.containsKey(playerUUID)) {
+                        ItemMeta itemMeta = actualItemIterated.getItemMeta();
                         final HashMap<ItemMeta, Integer> playerData = playerList.get(playerUUID);
-                        final Integer itemAmount = playerData.get(configItem.replace("items.", ""));
+                        System.out.println("PlayerData : " + playerData);
+                        final Integer itemAmount = playerData.get(itemMeta);
+                        System.out.println("ItemAmount : " + itemAmount);
                         if (itemAmount != null) {
                             final Integer newAmount = itemAmount + actualItemIterated.getAmount();
-                            //get the itemMeta of the item
-                            final ItemMeta actualitemMeta = actualItemIterated.getItemMeta();
-                            playerData.put(actualitemMeta, newAmount);
+                            playerData.put(itemMeta, newAmount);
                             playerList.put(playerUUID, playerData);
                         } else {
-                            final ItemMeta actualitemMeta = actualItemIterated.getItemMeta();
-                            playerData.put(actualitemMeta, actualItemIterated.getAmount());
+                            playerData.put(itemMeta, actualItemIterated.getAmount());
                             playerList.put(playerUUID, playerData);
                         }
                     } else {
-                        final ItemMeta actualitemMeta = actualItemIterated.getItemMeta();
+                        ItemMeta itemMeta = actualItemIterated.getItemMeta();
                         final HashMap<ItemMeta, Integer> playerData = new HashMap<>();
-                        playerData.put(actualitemMeta, actualItemIterated.getAmount());
+                        playerData.put(itemMeta, actualItemIterated.getAmount());
                         playerList.put(playerUUID, playerData);
                     }
                     iterator.remove();
                 }
             }
         }
-
-
     }
 
     /* When someone has respawned, check if the list of player is null
@@ -113,13 +81,27 @@ public class EventListener implements Listener {
         if (playerList.containsKey(playerUUID)) {
             for (Map.Entry<ItemMeta, Integer> entry : playerList.get(playerUUID).entrySet()) {
                 final ItemMeta itemMeta = entry.getKey();
-                final ItemStack itemStack = new ItemStack(Material.DIAMOND_SWORD);
-                itemStack.setItemMeta(itemMeta);
-                player.getInventory().addItem(itemStack);
-                itemData.remove(itemMeta);
+                final Integer itemAmount = entry.getValue();
+                //Get the displayname from the itemMeta
+                final String lore = itemMeta.getLore().get(1);
+                //check in the config if one item has the same lore
+                for (int i = 0; i < Main.configItemName.size(); i++) {
+                    final String configItem = Main.configItemName.get(i);
+                    String confItem = configItem.replace("items.", "");
+                    final List<String> configItemLore = Config.getItemLore(confItem);
+                    if (Objects.equals(lore, configItemLore.get(1))) {
+                        Material material = Material.valueOf(Config.getItemMaterial(confItem));
+                        System.out.println("material : " + material);
+                        System.out.println("itemAmount : " + itemAmount);
+                        ItemStack itemStack = new ItemStack(material, itemAmount);
+                        itemStack.setItemMeta(itemMeta);
+                        System.out.println("itemStack : " + itemStack);
+                        player.getInventory().addItem(itemStack);
+                    }
+                }
+                itemData.remove(entry.getKey());
                 playerList.remove(playerUUID);
             }
         }
     }
-
 }
